407c91787fabad98db0e036afb9eaede73b3d863
maks <maks1makrov@gmail.com>
2022-01-10 12:28:48 +0300

    1.11

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from bboard.models import Bd
+
+
 def index(request):
-    return HttpResponse("list of ads")
+    s = 'list of ads \r\n\r\n\r\n'
+    for bd in Bd.objects.order_by('published'):
+        s += bd.title + '\r\n' + bd.content + '\r\n\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
