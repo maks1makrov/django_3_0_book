00dd4a17cb36eca12783e984d77160d8b60dc913
maks <maks1makrov@gmail.com>
2022-01-25 14:02:46 +0300

    begin 1.12

--- bboard/views.py
+++ bboard/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from bboard.models import Bd
 
 
 def index(request):
-    s = 'list of ads \r\n\r\n\r\n'
-    for bd in Bd.objects.order_by('published'):
-        s += bd.title + '\r\n' + bd.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    template = loader.get_template('bboard/index.html')
+    bbs = Bd.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
